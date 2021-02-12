cf20485ed9ffeccacc1f7428a97573aa1af60f07
adamerstelle <adam@erstelle.ca>
2021-02-12 14:56:58 -0500

    make nested config classes static

--- src/main/java/com/example/configlistshapes/ConfigListShapesApplication.java
+++ src/main/java/com/example/configlistshapes/ConfigListShapesApplication.java
@@ -3,30 +3,33 @@ package com.example.configlistshapes;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Slf4j
 @SpringBootApplication
-public class ConfigListShapesApplication {
+public class ConfigListShapesApplication implements CommandLineRunner {
 
     @Autowired
     private ShapesConfig shapesConfig;
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigListShapesApplication.class, args);
+        SpringApplication app = new SpringApplication(ConfigListShapesApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
-    @PostConstruct
-    public void logResults() {
-        log.debug("We have {} shapes configured", shapesConfig.getShapes().size());
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("We have {} shapes configured", shapesConfig.getShapes().size());
         for(Shape shape : shapesConfig.getShapes()) {
-            log.debug("Shape is of type {} and class is instance of {}",
+            log.info("Shape is of type {} and class is instance of {}",
                     shape.getType(), shape.getClass().getSimpleName());
         }
     }
@@ -34,36 +37,36 @@ public class ConfigListShapesApplication {
     @Data
     @Component
     @ConfigurationProperties(prefix="app")
-    public class ShapesConfig {
+    public static class ShapesConfig {
         private List<Shape> shapes;
     }
 
     @Data
-    public class Shape {
+    public static class Shape {
         private String type;
     }
 
     @Data
-    public class Circle extends Shape {
+    public static class Circle extends Shape {
         private Integer radius;
         private Colors colors;
     }
 
     @Data
-    public class Rectangle extends Shape {
+    public static class Rectangle extends Shape {
         private Integer length;
         private Integer width;
         private Colors colors;
     }
 
     @Data
-    public class Triangle extends Shape {
+    public static class Triangle extends Shape {
         private Integer base;
         private Integer height;
     }
 
     @Data
-    public class Colors {
+    public static class Colors {
         private String fill;
         private String border;
     }
